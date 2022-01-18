c7861a29e4f6942fcfb29f2e764b13f5b6ac7a6f
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-18 10:16:51 -0300

    using the DropdownButton example by flutter doc, remenber to make a deep study about dart (map, types, ToList() method)

--- lib/app/widgets/select_currency.dart
+++ lib/app/widgets/select_currency.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SelectCurrency extends StatelessWidget {
-  const SelectCurrency({
-    Key? key,
-  }) : super(key: key);
+class SelectCurrency extends StatefulWidget {
+  const SelectCurrency({Key? key}) : super(key: key);
 
   @override
+  State<SelectCurrency> createState() => _SelectCurrencyState();
+}
+
+class _SelectCurrencyState extends State<SelectCurrency> {
+  String dropdownValue = 'USD';
+  var currencies = <String>['USD', 'EUR', 'BTC', "BRL"];
+  @override
   Widget build(BuildContext context) {
-    return DropdownButton<Object>(
-      items: [
-        DropdownMenuItem(
-          value: 'USD',
-          child: Text('USD'),
-        ),
-        DropdownMenuItem(
-          value: 'EUR',
-          child: Text('EUR'),
-        ),
-        DropdownMenuItem(
-          value: 'BTC',
-          child: Text('BTC'),
-        ),
-        DropdownMenuItem(
-          value: 'BRL',
-          child: Text('BRL'),
-        ),
-      ],
-      onChanged: (value) {},
+    return DropdownButton<String>(
+      value: dropdownValue,
+      elevation: 16,
+      style: const TextStyle(color: Colors.green),
+      underline: Container(
+        height: 2,
+        color: Colors.green,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      //REMENBER TO STUDIE THIS
+      items: currencies.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
