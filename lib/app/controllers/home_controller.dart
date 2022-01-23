506f820142296168cf3f5c16015e59e02f049493
gerssivaldosantos <canalandows@gmail.com>
2022-01-23 11:16:14 -0300

    business rules to controller

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -10,8 +10,29 @@ class HomeController {
   //base currency for conversion
   final TextEditingController fromText;
 
+  CurrencyModel toCurrency;
+  CurrencyModel fromCurrency;
+
   //constructor
-  HomeController(this.toText, this.fromText) {
-    currencies = <CurrencyModel>[];
+  HomeController({this.toText, this.fromText}) {
+    currencies = CurrencyModel.getCurrencies();
+    
+  }
+
+  void convert() {
+    String text = toText.text;
+    double value = double.tryParse(text) ?? 1.0;
+    double returnValue;
+    returnValue = 0;
+    if (fromCurrency.name == 'brl') {
+      returnValue = value * toCurrency.brl;
+    } else if (fromCurrency.name == 'usd') {
+      returnValue = value * toCurrency.usd;
+    } else if (fromCurrency.name == 'eur') {
+      returnValue = value * toCurrency.eur;
+    } else if (fromCurrency.name == 'btc') {
+      returnValue = value * toCurrency.btc;
+    }
+    fromText.text = returnValue.toStringAsFixed(2);
   }
 }
