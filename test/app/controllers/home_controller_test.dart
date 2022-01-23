9422e06bb3961caf55560091e1006d538b1695e1
gerssivaldosantos <canalandows@gmail.com>
2022-01-23 11:16:50 -0300

    new test for controller

---
+++ test/app/controllers/home_controller_test.dart
@@ -0,0 +1,17 @@
+import 'package:currency_converter/app/controllers/home_controller.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+  final homeController = HomeController(toText: toText, fromText: fromText);
+
+  test('Should do convert from brl to usd', () {
+    toText.text = '2.0';
+    homeController.toCurrency = homeController.currencies[0];
+    homeController.fromCurrency = homeController.currencies[1];
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+}
