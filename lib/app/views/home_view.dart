89bb2c59e18499d2e24a968d662e166849c40f3e
gerssivaldosantos <canalandows@gmail.com>
2022-01-25 06:41:56 -0300

    SingleChildScrollView and new instances

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,5 +1,5 @@
+import 'package:currency_converter/app/widgets/currency_box.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomeView extends StatelessWidget {
   @override
@@ -8,57 +8,48 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Spacer(
-            flex: 3,
-          ),
-          SizedBox(
-              width: MediaQuery.of(context).size.width * 0.5,
-              child: Image.asset('assets/images/logo.png')),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-            child: Text('Currency Converter'),
-          ),
-          //THE ROW INPUTS
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              DropdownButton(items: []),
+      body:
+          // This is used to make the screen responsive when the keyboard
+          // is opened or closed
+          SingleChildScrollView(
+        child: SizedBox(
+          //define the height and width by screen size
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(
+                flex: 1,
+              ),
               SizedBox(
-                width: 20,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Image.asset('assets/images/logo.png')),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+                child: Text('Currency Converter'),
               ),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter Amount',
-                ),
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-              )),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Convert',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+              CurrencyBox(),
+              CurrencyBox(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Convert',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Spacer(
+                flex: 5,
+              ),
+            ],
           ),
-          Spacer(
-            flex: 5,
-          ),
-        ],
+        ),
       ),
     );
   }
